c8f109a05ad6520d8119d731821e84d39cfd269e
Eric Hertwig <eric@ehdev.de>
2019-12-24 22:53:18 +0100

    Add callback handler

---
+++ src/Handler/CallbackHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPSharkTank\AnonymizeBundle\Handler;
+
+class CallbackHandler implements HandlerInterface
+{
+    public function getName(): string
+    {
+        return 'callback';
+    }
+
+    public function process($object, array $options)
+    {
+        if (isset($options['method'])) {
+            $callable = [$object, $options['method']];
+            if (is_callable($callable)) {
+                return $callable();
+            }
+        }
+
+        return call_user_func($options['callback']);
+    }
+}
