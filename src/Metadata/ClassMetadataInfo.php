6a4423f9798a0f2deca6c2264b47117fefc9ba45
Eric Hertwig <eric@ehdev.de>
2019-12-24 22:36:10 +0100

    Add Metadata

---
+++ src/Metadata/ClassMetadataInfo.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPSharkTank\AnonymizeBundle\Metadata;
+
+class ClassMetadataInfo
+{
+    public $className;
+
+    /** @var array<string, PropertyMetadata> */
+    public $propertyMetadata = [];
+
+    public $reflection;
+
+    public function __construct($className)
+    {
+        $this->className = $className;
+
+        $this->reflection = new \ReflectionClass($this->className);
+    }
+
+    public function addPropertyMetadata(PropertyMetadata $metadata): void
+    {
+        $this->propertyMetadata[$metadata->getName()] = $metadata;
+    }
+
+    public function getPropertyMetadata(): array
+    {
+        return $this->propertyMetadata;
+    }
+
+    public function __sleep()
+    {
+        return [
+            'className',
+            'propertyMetadata',
+        ];
+    }
+
+    public function __wakeup()
+    {
+        $this->reflection = new \ReflectionClass($this->className);
+    }
+}
---
+++ src/Metadata/PropertyMetadata.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPSharkTank\AnonymizeBundle\Metadata;
+
+class PropertyMetadata
+{
+    private $reflection;
+
+    private $name;
+
+    private $className;
+
+    private $type;
+
+    private $options = [];
+
+    public function __construct(string $class, string $propertyName, string $type = 'text')
+    {
+        $this->className = $class;
+        $this->name = $propertyName;
+        $this->type = $type;
+
+        $this->reflection = new \ReflectionProperty($this->className, $propertyName);
+        $this->reflection->setAccessible(true);
+    }
+
+    public function setValue($object, $value): void
+    {
+        $this->reflection->setValue($object, $value);
+    }
+
+    public function getValue($object)
+    {
+        return $this->reflection->getValue($object);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setOptions(array $options)
+    {
+        $this->options = $options;
+    }
+
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    public function __sleep()
+    {
+        return [
+            'name',
+            'className',
+            'type',
+            'options',
+        ];
+    }
+
+    public function __wakeup()
+    {
+        $this->reflection = new \ReflectionProperty($this->className, $this->name);
+        $this->reflection->setAccessible(true);
+    }
+}
