02ddae2c8aa20c1cd2dbf0a7a13cf7bd38bbf65b
Eric Hertwig <eric@ehdev.de>
2019-12-24 22:40:17 +0100

    Add cache loader

---
+++ src/Loader/CachingLoader.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPSharkTank\AnonymizeBundle\Loader;
+
+use PHPSharkTank\AnonymizeBundle\Metadata\ClassMetadataInfo;
+use PHPSharkTank\AnonymizeBundle\Exception\MetadataNotFoundException;
+use Psr\Cache\CacheItemPoolInterface;
+
+final class CachingLoader implements LoaderInterface
+{
+    /**
+     * @var LoaderInterface
+     */
+    private $delegate;
+    /**
+     * @var CacheItemPoolInterface
+     */
+    private $pool;
+
+    public function __construct(LoaderInterface $delegate, CacheItemPoolInterface $pool)
+    {
+        $this->delegate = $delegate;
+        $this->pool = $pool;
+    }
+
+    public function getMetadataFor(string $className): ClassMetadataInfo
+    {
+        $item = $this->pool->getItem(md5($className));
+        if ($item->isHit()) {
+            return $item->get();
+        }
+
+        try {
+            $metadata = $this->delegate->getMetadataFor($className);
+            $item->set($metadata);
+            $this->pool->save($item);
+
+            return $metadata;
+        } catch (MetadataNotFoundException $e) {
+            throw $e;
+        }
+    }
+}
